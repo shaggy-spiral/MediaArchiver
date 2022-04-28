ad8bd7fe5328999094150aec26bcb10d723144e6
Tiago Sobral <sobral@pop-os.localdomain>
2022-04-28 23:28:22 +0100

    Get photo year and month

--- src/main/java/Archiver.java
+++ src/main/java/Archiver.java
@@ -3,12 +3,14 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifIFD0Directory;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -46,20 +48,15 @@ public class Archiver {
                     throw new RuntimeException(e);
                 }
 
+                Date creationDate = null;
+                ExifIFD0Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+                creationDate = directory.getDate(ExifIFD0Directory.TAG_DATETIME);
 
-                Iterable<Directory> directories = metadata.getDirectories();
-                for (Directory directory: directories) {
-                    Collection<Tag> tags = directory.getTags();
-                    for (Tag tag : tags) {
-                        if (tag.getTagName() == "Date/Time") {
-                            System.out.println(tag.getDescription());
-                        }
-
-                    }
-                }
-
-                System.out.println();
-
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(creationDate);
+                Integer photoYear = calendar.get(Calendar.YEAR);
+                Integer photoMonth = calendar.get(Calendar.MONTH) + 1;
+                System.out.println(photoYear + "/" + photoMonth);
             }
         }
     }
